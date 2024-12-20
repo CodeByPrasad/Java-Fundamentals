932af67ec7dff06e482dfdec97ef0f3e4115d209
prasad <prasad2082001@gmail.com>
2024-12-20 11:55:36 +0530

    For loop example

---
+++ src/com/practice/ForLoop.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+public class ForLoop {
+
+    public static void main(String[] args){
+        int n = 10;
+        for (int i = 0; i<n; i++){
+            System.out.println(i);
+        }
+
+    }
+}
