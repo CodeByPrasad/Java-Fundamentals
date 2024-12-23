2921f3d5c20ac802bea3619676f7859b6682c0b8
prasad <prasad2082001@gmail.com>
2024-12-23 14:42:06 +0530

    string methods

--- src/com/practice/TwoDimensionArray.java
+++ src/com/practice/TwoDimensionArray.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TwoDimensionArray {
     public static void main(String[] args){
 
-        int[][] arr = new int[5][];
+        int[][] arr = new int[3][];
         Scanner sc = new Scanner(System.in);
         for (int i=0;i<arr.length;i++)
         {
