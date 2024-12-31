3da294eeb16ef394161414b4999bc53897702bda
prasad <prasad2082001@gmail.com>
2024-12-31 12:32:51 +0530

    LinkedHashMap in  java

---
+++ src/com/collections/LinkedHashmap.java
@@ -0,0 +1,14 @@
+package com.collections;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashmap {
+    public static void main(String[] args){
+
+        LinkedHashMap<Integer, String > lhm = new LinkedHashMap<>();
+
+        lhm.put(1,"Prasad");
+        lhm.put(2,"Shivaji");
+        System.out.println(lhm);
+    }
+}
