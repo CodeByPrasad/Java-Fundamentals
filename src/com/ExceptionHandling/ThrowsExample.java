7f91f66d3e0afeb0b4b465cd0af92314ca7b9375
prasad <prasad2082001@gmail.com>
2025-01-03 13:45:21 +0530

    Exception

--- src/com/ExceptionHandling/ThrowsExample.java
+++ src/com/ExceptionHandling/ThrowsExample.java
@@ -1,13 +1,26 @@
 package com.ExceptionHandling;
 
 public class ThrowsExample {
-    static void validateAge(int age) throws Exception {
+
+    static void validateAge(int age) throws ArithmeticException {
       if (age < 18){
-          throw new Exception("Cant give access to age below 18");
+          throw new ArithmeticException("Cant give access to age below 18");
       }
         System.out.println("Access granted !");
+        int arr[] = {1,2,3,4,5};
+        for(int i : arr){
+            System.out.println(i);
+        }
+        if (arr.length < 6) {
+            throw new IndexOutOfBoundsException("Index out of bound");
+        }
     }
+
     public static void main(String[] args) throws Exception {
-        validateAge(18);
+        try{
+            validateAge(19);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
